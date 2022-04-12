bc650d6514ce900ca04a02301b31087a6c94270b
Кондакова Татьяна <rotaru2803@rambler.ru>
2022-04-12 12:50:52 +0500

    add all

---
+++ src/test/java/GeoServiceImplTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
+import ru.netology.geo.GeoServiceImpl;
+import ru.netology.geo.GeoService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GeoServiceImplTest {
+    GeoService sut = new GeoServiceImpl();
+
+    @Test
+    public void locationByIPTest() {
+
+        Location expected = new Location("Moscow", Country.RUSSIA, "Lenina", 15);
+        Location result = sut.byIp("172.0.32.11");
+        assertEquals(expected.getCountry(), result.getCountry());
+    }
+
+    @Test
+    public void exceptionTest() {
+        assertThrows(RuntimeException.class, () -> sut.byCoordinates(1.2, 3.4));
+    }
+
+}
---
+++ src/test/java/LocalizationServiceImplTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import ru.netology.entity.Country;
+import ru.netology.i18n.LocalizationServiceImpl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LocalizationServiceImplTest {
+
+    @Test
+    public void greetingRussiaTest() {
+        LocalizationServiceImpl sut = new LocalizationServiceImpl();
+        Country country = Country.RUSSIA;
+        String expected = "Добро пожаловать";
+        String result = sut.locale(country);
+        assertEquals(expected, result);
+    }
+
+}
